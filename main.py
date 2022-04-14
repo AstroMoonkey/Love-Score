65d73eec1041a181ee816d4a44a496d1cdc2f3c9
holsar <31683454+AstroMoonkey@users.noreply.github.com>
2022-04-14 19:28:34 +0000

    created program. but missed the requirements of the exercised. the score is not sum, but rather it should be stitched together.

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ name2 = input("What is their name? \n")
 
 names = name1.replace(" ","")+ name2.replace(" ","")
 names = names.lower()
-print(names)
-# print(names.count("t"))
-# print(names.count("r"))
-# print(names.count("u"))
-# print(names.count("e"))
-# print(names.count("l"))
-# print(names.count("o"))
-# print(names.count("v"))
-# print(names.count("e"))
+#print(names)
+#print(names.count("t"))
+#print(names.count("r"))
+#print(names.count("u"))
+#print(names.count("e"))
+#print(names.count("l"))
+#print(names.count("o"))
+#print(names.count("v"))
+#print(names.count("e"))
 tally = names.count("t") + names.count("r") + names.count("u") + names.count("e") + names.count("l") + names.count("o") + names.count("v") + names.count("e")
 
 #print(tally)
 
-if tally < 10 or tally > 90:
+if tally < 10 or tally > 90: # **less than 10** or **greater than 90**
   print(f"your score is {tally} you two are like coke and mentols together")
-elif tally > 40 and tally < 50:
+elif tally > 40 and tally < 50: # **between 40** and **50**
   print(f"your score is {tally} you are alright together")
 else:
   print(f"your score is {tally}. meh!")
