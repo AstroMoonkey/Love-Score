834b377dfb21da0b0b7b3816c65e8c310f20bdb4
holsar <31683454+AstroMoonkey@users.noreply.github.com>
2022-04-14 19:08:04 +0000

    Completed the program.

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ name2 = input("What is their name? \n")
 names = name1.replace(" ","")+ name2.replace(" ","")
 names = names.lower()
 print(names)
+# print(names.count("t"))
+# print(names.count("r"))
+# print(names.count("u"))
+# print(names.count("e"))
+# print(names.count("l"))
+# print(names.count("o"))
+# print(names.count("v"))
+# print(names.count("e"))
+tally = names.count("t") + names.count("r") + names.count("u") + names.count("e") + names.count("l") + names.count("o") + names.count("v") + names.count("e")
 
+#print(tally)
 
-
-
+if tally < 10 or tally > 90:
+  print(f"your score is {tally} you two are like coke and mentols together")
+elif tally > 40 and tally < 50:
+  print(f"your score is {tally} you are alright together")
+else:
+  print(f"your score is {tally}. meh!")
